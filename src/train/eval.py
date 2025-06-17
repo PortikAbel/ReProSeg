remove metrics from loss calculation + correct mIoU calculation

src/train/eval.py
@@ -59,7 +59,7 @@ def acc_from_cm(cm: torch.Tensor) -> float:
 
     return 1 if total == 0 else correct / total
 
-def iou_from_cm(cm: torch.Tensor) -> float:
+def intersection_and_union_from_cm(cm: torch.Tensor) -> float:
     """
     Compute the IoU from the confusion matrix
     :param cm: confusion matrix
@@ -70,5 +70,4 @@ def iou_from_cm(cm: torch.Tensor) -> float:
     intersection = cm.diagonal()
     union = (cm + cm.T).sum(dim=0) - intersection
 
-    iou = intersection / union
-    return iou.mean().item() if iou.numel() > 0 else 1
+    return intersection, union

src/train/loss.py
@@ -3,7 +3,6 @@ import torch.nn.functional as F
 
 from utils.log import Log
 from model.model import TrainPhase
-from .eval import compute_cm, acc_from_cm
 
 class LossWeights:
     def __init__(
@@ -53,14 +52,11 @@ def calculate_loss(
         loss += weights.uniformity * uni_loss
         loss += weights.variance * var_loss
 
-    acc = 0.0
     if train_phase is not TrainPhase.PRETRAIN:
         softmax_inputs = torch.log1p(out**2)
         class_loss = criterion(F.log_softmax((softmax_inputs), dim=1), ys.squeeze())
 
         loss += weights.classification * class_loss
-
-        acc = acc_from_cm(compute_cm(out, ys))
 
     if print:
         with torch.no_grad():
@@ -73,8 +69,7 @@ def calculate_loss(
                     f"LV:{var_loss.item():.3f}, " +
                     f"LC:{class_loss.item():.3f}, " +
                     f"L:{loss.item():.3f}, " +
-                    f"num_scores>0.1:{torch.count_nonzero(torch.relu(pooled-0.1),dim=1).float().mean().item():.1f}" +
-                    f", Ac:{acc:.3f}"
+                    f"num_scores>0.1:{torch.count_nonzero(torch.relu(pooled-0.1),dim=1).float().mean().item():.1f}"
                 ),
                 refresh=False,
             )
@@ -87,7 +82,7 @@ def calculate_loss(
             log.tb_scalar(f"Loss/{phase_string}/LC", class_loss.item(), iteration)
             log.tb_scalar(f"Loss/{phase_string}/L", loss.item(), iteration)
 
-    return loss, acc
+    return loss
 
 
 def jensen_shannon_divergence(x):

src/train/test_step.py
@@ -22,7 +22,7 @@ def eval(
     net.eval()
     eval_info = dict()
 
-    n_classes = args.num_classes
+    n_classes = args.num_classes - 1
     cm = torch.zeros((n_classes, n_classes), dtype=int).to(args.device)
     abstained = 0.0
     

src/train/train_step.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import argparse
 from model.model import ReProSeg, TrainPhase
 from model.optimizers import OptimizerSchedulerManager
+from train.eval import compute_cm, acc_from_cm, intersection_and_union_from_cm
 from train.loss import LossWeights, calculate_loss
 from utils.log import Log
 
@@ -27,6 +28,8 @@ def train(
     train_info = dict()
     total_loss = 0.0
     total_acc = 0.0
+    total_intersections_by_class = torch.zeros(args.num_classes - 1).to(args.device)
+    total_unions_by_class = torch.zeros(args.num_classes - 1).to(args.device)
 
     iters = len(train_loader)
     # Show progress on progress bar.
@@ -63,7 +66,7 @@ def train(
         aspp_features, pooled, out = net(torch.cat([xs1, xs2]))
         prototype_activations[i, :, :, :, :] = pooled
 
-        loss, acc = calculate_loss(
+        loss = calculate_loss(
             log,
             aspp_features,
             pooled,
@@ -83,8 +86,14 @@ def train(
         optimizer_scheduler_manager.step(net.train_phase, epoch - 1 + (i / iters))
 
         with torch.no_grad():
-            total_acc += acc
             total_loss += loss.item()
+
+            if net.train_phase is not TrainPhase.PRETRAIN:
+                cm = compute_cm(out, ys)
+                total_acc += acc_from_cm(cm)
+                i, u = intersection_and_union_from_cm(cm)
+                total_intersections_by_class += i
+                total_unions_by_class += u
 
         if net.train_phase is not TrainPhase.PRETRAIN:
             with torch.no_grad():
@@ -100,8 +109,10 @@ def train(
                         torch.clamp(net.layers.classification_layer.bias.data, min=0.0)
                     )
 
-    train_info["train_accuracy"] = total_acc / float(i + 1)
     train_info["loss"] = total_loss / float(i + 1)
+    train_info["train_accuracy"] = total_acc / float(i + 1)
+    train_info["train_miou"] = (total_intersections_by_class / total_unions_by_class).mean().item()
+    train_info["train_iou_by_class"] = (total_intersections_by_class / total_unions_by_class).detach().cpu().numpy()
     train_info["prototype_activations"] = (
         prototype_activations.view((-1, net.layers.num_prototypes))
         .detach()