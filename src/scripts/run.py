7b2562ccad7ceb4ca61c2abf3c2d32ffe0df5c71
annamaria <csigabiga08@gmail.com>
2025-07-29 12:57:45 +0300

    add omegaconf wrapper

--- src/scripts/run.py
+++ src/scripts/run.py
@@ -1,7 +1,7 @@
 from data.dataloader import DataLoader, DoubleAugmentDataLoader, PanopticPartsDataLoader
 from model.model import ReProSeg
-from utils.args import ModelTrainerArgumentParser
 from utils.log import Log
+from utils.args import ConfigWrapper
 
 import hydra
 from omegaconf import DictConfig, OmegaConf
@@ -20,7 +20,7 @@ def set_rand_state(seed: int):
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
 
-def set_device(gpu_ids: str, disable_gpu: bool = False) -> tuple[str, list]:
+def set_device(gpu_ids: str, disable_gpu: bool = False) -> tuple[torch.device, list]:
     """
     Set the device to use for training.
 
@@ -32,11 +32,11 @@ def set_device(gpu_ids: str, disable_gpu: bool = False) -> tuple[str, list]:
     device_ids = [int(gpu_id) for gpu_id in (gpu_ids.split(",") if gpu_ids else [])]
 
     if disable_gpu or not torch.cuda.is_available():
-        return "cpu", []
+        return torch.device("cpu"), []
     if len(device_ids) == 1:
-        return f"cuda:{gpu_ids}", device_ids
+        return torch.device(f"cuda:{gpu_ids}"), device_ids
     if len(device_ids) == 0:
-        device = "cuda"
+        device = torch.device("cuda")
         print("CUDA device set without id specification", flush=True)
         device_ids.append(torch.cuda.current_device())
         return device, device_ids
@@ -45,65 +45,67 @@ def set_device(gpu_ids: str, disable_gpu: bool = False) -> tuple[str, list]:
         "but we didn't test that, so we recommend to use only 1 GPU.",
         flush=True,
     )
-    return "cuda:" + str(device_ids[0]), device_ids
+    return torch.device("cuda:" + str(device_ids[0])), device_ids
 
 @hydra.main(version_base=None, config_path="../utils", config_name="config.yaml")
 def main(cfg: DictConfig):
-    OmegaConf.set_struct(cfg, False)  # disable strict structure
 
     # Setup logger
     log = Log(cfg.log_dir, __name__)
     log.info(f"Config:\n{OmegaConf.to_yaml(cfg)}")
 
+    # wrap omegaconf object so custom type objects can be added later
+    args = ConfigWrapper(cfg)
+
     # Set random seed
-    set_rand_state(cfg.seed)
+    set_rand_state(args.seed)
 
     # Set device
-    cfg.device, cfg.device_ids = set_device(cfg.gpu_ids, cfg.disable_gpu)
-    log.info(f"Device used: {type(cfg.device)} {f'with id {cfg.device_ids}' if len(cfg.device_ids) > 0 else ''}")
+    args.device, args.device_ids = set_device(args.gpu_ids, args.disable_gpu)
+    log.info(f"Device used: {type(args.device)} {f'with id {args.device_ids}' if len(args.device_ids) > 0 else ''}")
 
     # Load checkpoint-specific logging directory
-    if cfg.model_checkpoint is not None:
-        cfg.log_dir = str(Path(cfg.model_checkpoint).parent.parent)
+    if args.model_checkpoint is not None:
+        args.log_dir = str(Path(args.model_checkpoint).parent.parent)
     
     # Handle default jsd loss fallback
     if (
-        cfg.jsd_loss == 0.0
-        and cfg.tanh_loss == 0.0
-        and cfg.unif_loss == 0.0
-        and cfg.variance_loss == 0.0
+        args.jsd_loss == 0.0
+        and args.tanh_loss == 0.0
+        and args.unif_loss == 0.0
+        and args.variance_loss == 0.0
     ):
         log.info("No loss function specified. Using JSD loss by default")
-        cfg.jsd_loss = 5.0
+        args.jsd_loss = 5.0
 
     # Create the dataloaders
-    train_loader = DoubleAugmentDataLoader(cfg)
-    test_loader = DataLoader("test", cfg)
-    train_loader_visualization = DataLoader("train", cfg)
-    panoptic_parts_loader = PanopticPartsDataLoader("train", cfg)
+    train_loader = DoubleAugmentDataLoader(args)
+    test_loader = DataLoader("test", args)
+    train_loader_visualization = DataLoader("train", args)
+    panoptic_parts_loader = PanopticPartsDataLoader("train", args)
 
-    cfg.num_classes = len(train_loader.dataset.classes)
+    args.num_classes = len(train_loader.dataset.classes)
     # Model
-    net = ReProSeg(args=cfg, log=log).to(device=torch.device(cfg.device))
+    net = ReProSeg(args=args, log=log).to(device=args.device)
 
-    if not cfg.skip_training:
+    if not args.skip_training:
         from train.trainer import train_model
 
         try:
-            train_model(net, train_loader, test_loader, log, cfg)
+            train_model(net, train_loader, test_loader, log, args)
         except Exception as e:
             log.exception(e)
 
-    if cfg.visualize_prototypes:
+    if args.visualize_prototypes:
         from visualize.visualizer import ModelVisualizer
 
-        visualizer = ModelVisualizer(net, cfg, log, k=cfg.visualize_top_k)
+        visualizer = ModelVisualizer(net, args, log, k=args.visualize_top_k)
         visualizer.visualize_prototypes(train_loader_visualization)
 
-    if cfg.consistency_score:
+    if args.consistency_score:
         from visualize.interpretability import ModelInterpretability
         interpretability = ModelInterpretability(
-            net, cfg, log, consistency_threshold=cfg.consistency_threshold)
+            net, args, log, consistency_threshold=args.consistency_threshold)
         interpretability.compute_prototype_consistency_score(panoptic_parts_loader)
 
     log.close()
