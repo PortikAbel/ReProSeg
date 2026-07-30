5ec8cc84fa3a9d3abf5623bef0cf7531b1fb410b
zalan bodo <zalab.bodo@gmail.com>
2026-07-30 18:21:40 +0300

    minor mod in worker_init_fn of dataloader

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 import torch
 from torch.utils.data import DataLoader as TorchDataLoader
 from torch.utils.data import Dataset as TorchDataset
@@ -12,9 +13,11 @@ class DataLoader(TorchDataLoader):
     dataset: TorchDataset
     to_shuffle: bool = False
     to_drop_last: bool = False
+    cfg: ReProSegConfig
 
     def __init__(self, dataset: TorchDataset, cfg: ReProSegConfig):
         self.dataset = dataset
+        self.cfg = cfg
         super().__init__(
             self.dataset,
             batch_size=cfg.data.batch_size,
@@ -22,6 +25,13 @@ class DataLoader(TorchDataLoader):
             sampler=None,
             pin_memory=torch.cuda.is_available(),
             num_workers=cfg.data.num_workers,
-            worker_init_fn=lambda worker_id: np.random.seed(cfg.env.seed + worker_id),
+            # worker_init_fn=lambda worker_id: np.random.seed(cfg.env.seed + worker_id),
+            worker_init_fn=self._seed_worker,
             drop_last=self.to_drop_last,
         )
+
+    def _seed_worker(self, worker_id):
+        worker_seed = self.cfg.env.seed + worker_id
+        np.random.seed(worker_seed)
+        random.seed(worker_seed)
+    
