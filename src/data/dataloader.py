f030a47b5f1ed55ada441757f1de2d4342f47d14
zalan bodo <zalab.bodo@gmail.com>
2026-07-31 16:58:52 +0300

    dataloader purge

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,7 +1,5 @@
 import random
-
 import numpy as np
-import random
 import torch
 from torch.utils.data import DataLoader as TorchDataLoader
 from torch.utils.data import Dataset as TorchDataset
@@ -23,7 +21,6 @@ class DataLoader(TorchDataLoader):
     dataset: TorchDataset
     to_shuffle: bool = False
     to_drop_last: bool = False
-    cfg: ReProSegConfig
 
     def __init__(self, dataset: TorchDataset, cfg: DataConfig):
         self.dataset = dataset
@@ -34,16 +31,8 @@ class DataLoader(TorchDataLoader):
             shuffle=self.to_shuffle,
             sampler=None,
             pin_memory=torch.cuda.is_available(),
-            num_workers=cfg.data.num_workers,
-            # worker_init_fn=lambda worker_id: np.random.seed(cfg.env.seed + worker_id),
-            worker_init_fn=self._seed_worker,
             num_workers=cfg.num_workers,
             worker_init_fn=seed_worker,
             drop_last=self.to_drop_last,
         )
-
-    def _seed_worker(self, worker_id):
-        worker_seed = self.cfg.env.seed + worker_id
-        np.random.seed(worker_seed)
-        random.seed(worker_seed)
     
