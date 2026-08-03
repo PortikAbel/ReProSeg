4bf8e18a7c1fb95d84c9503722befdcb2c3b3db6
annamari <csigabiga08@gmail.com>
2026-08-03 20:02:29 +0300

    add start slurm example for consistency score calculation

--- src/scripts/start_slurm_job.sh
+++ src/scripts/start_slurm_job.sh
@@ -10,7 +10,12 @@ echo "CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-none}"
 
 cd "${SLURM_SUBMIT_DIR:?SLURM_SUBMIT_DIR is not set}"
 # uv run python src/scripts/run.py "$@"
+
 # uv run python src/scripts/run.py training.skip_training=true +evaluation.consistency_score.calculate=true +evaluation.consistency_score.threshold=0.8
-uv run python src/scripts/protoseg_consistency.py \
-    model.checkpoint=/home/annamari/ProtoSeg-checkpoints/cityscapes_no_kld_imnet_4_16/checkpoints/push_best.pth \
-    +evaluation.consistency_score.threshold=0.8
+
+uv run python -m visualize.consistency \
+  /home/annamari/ProtoSeg-checkpoints/cityscapes_kld_imnet_4_16v2/checkpoints/push_best.pth \
+  --data-path /data/datasets/Cityscapes \
+  --quantile 0.8 \
+  --threshold 0.8 \
+  --output-dir consistency_results
