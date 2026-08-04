844b8275c89dd46c3924b980d95bd56fb43327e9
annamari <csigabiga08@gmail.com>
2026-08-04 09:22:19 +0300

    add slurm run reproseg consistency score example

--- src/scripts/start_slurm_job.sh
+++ src/scripts/start_slurm_job.sh
@@ -13,9 +13,20 @@ cd "${SLURM_SUBMIT_DIR:?SLURM_SUBMIT_DIR is not set}"
 
 # uv run python src/scripts/run.py training.skip_training=true +evaluation.consistency_score.calculate=true +evaluation.consistency_score.threshold=0.8
 
-uv run python -m visualize.consistency \
-  /home/annamari/ProtoSeg-checkpoints/cityscapes_kld_imnet_4_16v2/checkpoints/push_best.pth \
-  --data-path /data/datasets/Cityscapes \
-  --quantile 0.8 \
-  --threshold 0.8 \
-  --output-dir consistency_results
+# for q in 0.7 0.8 0.9; do
+#   uv run python -m visualize.consistency \
+#     /home/annamari/ProtoSeg-checkpoints/cityscapes_kld_imnet_4_16v2/checkpoints/push_best.pth \
+#     --data-path /data/datasets/Cityscapes \
+#     --quantile "$q" \
+#     --threshold 0.8 \
+#     --output-dir consistency_results_protoseg
+# done
+
+for q in 0.7 0.8 0.9; do
+  uv run python -m visualize.consistency \
+    /home/annamari/ReProSeg-checkpoints/net_trained_best_miou \
+    --data-path /data/datasets/Cityscapes \
+    --quantile "$q" \
+    --threshold 0.8 \
+    --output-dir consistency_results_reproseg
+done
